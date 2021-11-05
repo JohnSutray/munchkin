0f78f10a49053aaae48c3cd6cc4ed589d87a82d8
Andrey Zobkov <andrey.zobkov@tobania.be>
2021-11-05 22:37:12 +0300

    removed unused code

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'apps/api/src/app/app.module';
@@ -10,8 +5,6 @@ import { AppModule } from 'apps/api/src/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port, () => Logger.log('Listening at http://localhost:' + port));
 }
